808881aafc20b46c8f6fff700f2b5e9bf5e4d27b
Rolando Cosme <rolandocosme@Rolandos-MBP.home>
2015-12-12 23:09:42 -0500

    began score keeping functionality

--- js/events.js
+++ js/events.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
   var choices = ["rock", "paper", "scissors"];
+  
 
 
  $(".btn-success").on("click", function (){
   var ranNum = Math.floor(Math.random() * choices.length);
   var computerChoice = choices[ranNum];
+  var computerScoreUp = 0
+  var playerScoreUp = 0
+  var tied = 0
       $("#computerSelection").html(computerChoice);
       $("#playerSelection").html(this.id);
       if ($(this).attr("id") === computerChoice) {
        console.log("tied");
       }
       else if ($(this).attr("id") === "rock" && computerChoice === "paper") {
-       console.log("your computer wins");
+        console.log("your computer wins");
+        computerScoreUp++;
+        $("#computerScore").html(computerScoreUp)  
+       // function(currentValue) {
+       //  return $("#computerScore").html(currentValue || 0) + 1;
+      //return $("#computerScore").html++;
       }
       else if ($(this).attr("id") === "rock" && computerChoice === "scissors") {
        console.log("you win");
